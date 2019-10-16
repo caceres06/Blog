a7b2a899322273dca9617e395568c3875c64c3c1
caceres06 <37302560+caceres06@users.noreply.github.com>
2019-10-16 02:30:19 -0600

    se modifico js

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,33 @@
-function CambiarDisplay(){
-    var lunes=document.querySelector(".lun")
-    var martes=document.querySelector(".mar")
-    var miercoles=document.querySelector(".mier")
-    var jueves=document.querySelector(".jue")
-    var viernes=document.querySelector(".vie")
-
-document.querySelector(".ma").addEventListener("click", ()=> {
-    lunes.classList.toggle("mar")
-})
+function muestraLunes(id){
+    if (document.getElementById){
+    var el = document.getElementById(id); 
+    el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
 }
 
-window.onload=function()
-{
-    CambiarDisplay();
+function muestraMartes(id){
+    if (document.getElementById){
+    var dia2 = document.getElementById(id); 
+    dia2.style.display = (dia2.style.display == 'none') ? 'block' : 'none';
+    }
 }
+function muestraMiercoles(id){
+    if (document.getElementById){
+    var dia3 = document.getElementById(id); 
+    dia3.style.display = (dia3.style.display == 'none') ? 'block' : 'none';
+    }
+}
+
+function muestrJueves(id){
+    if (document.getElementById){
+    var el = document.getElementById(id); 
+    el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+
+window.onload = function(){
+    muestraLunes('1');
+    muestraMartes('2');
+    muestraMiercoles('3');
+}
+
